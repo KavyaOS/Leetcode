d493fa4115f4c1c4bc6360a8a62ea6e87b7b6883
Kavya O S <kavya.os15@gmail.com>
2021-09-03 14:09:49 -0700

    Solution accepted with Big O optimized

--- Binary_tree/LevelorderTraversal.py
+++ Binary_tree/LevelorderTraversal.py
@@ -5,28 +5,23 @@ class Node:
         self.right = None
 
 class Solution:
-    '''Worst case complexities: Space: O(N), where N being the number of nodes in the given tree.
-     Time: O(M*N), where M being number of levels of a tree, N being maximum number of nodes in a certain level'''
+    '''Worst case complexities: 
+        Space (To hold result): O(l*n), where l being the number of levels of a binary tree and n being the number of nodes at a certain level
+        Time: O(2^l) or O(n), since in worst case n can go upto 2^l'''
     def levelOrder(self, root):
         if root is None:
             return root
         result = []
-        result.append([root.val])
-        curr_layer = []
-        curr_layer.append(root)
-        while curr_layer:
-            new_layer = []
-            new_level = []
-            for node in curr_layer:
-                if node.left:
-                    new_layer.append(node.left)
-                    new_level.append(node.left.val)
-                if node.right:
-                    new_layer.append(node.right)
-                    new_level.append(node.right.val)
-            if new_level:
-                result.append(new_level)
-            curr_layer = new_layer
+        def helper_traversal(level, node):
+            '''Helper function which returns nothing but arranges result in proper format'''
+            if node:
+                if len(result)<=level:
+                    result.append([node.val])
+                else:
+                    result[level].append(node.val)
+                helper_traversal(level+1, node.left)
+                helper_traversal(level+1, node.right)
+        helper_traversal(0, root)
         return result
 
 tree = Node(3)
@@ -35,8 +30,8 @@ child2 = Node(20)
 child3 = Node(15)
 child4 = Node(7)
 
-tree.right = child1
-child1.left = child2
+tree.left = child1
+tree.right = child2
 child2.left = child3
 child2.right = child4
 
