e2642747bc86f31dd5f4a6cc6528425228d6dcef
Kavya O S <kavya.os15@gmail.com>
2021-10-27 10:57:12 -0700

    solution accepted

--- Algorithms/Dynamic_Programming/Longest_Palindromic_Subsequence.py
+++ Algorithms/Dynamic_Programming/Longest_Palindromic_Subsequence.py
@@ -5,16 +5,14 @@ class Solution(object):
         DP = np.zeros((string_length, string_length))
         for row in range(rows):
             DP[row][row] = 1
-        for substring_length in range(2, len(s)+1):
-            for row in range(0, string_length - substring_length + 1):
-                col = row + substring_length - 1
+        for k in range(1, len(s)):
+            for row in range(rows-k):
+                col = row + k
                 if s[row] == s[col]:
-                    if string_length==2:
-                        DP[row][col] = 2
-                    else:
-                        DP[row][col] = 2 + DP[row+1][col-1]
+                    DP[row][col] = 2 + DP[row+1][col-1]
                 else:
                     DP[row][col] = max(DP[row+1][col], DP[row][col-1])
-        return DP[0][-1]
 
-print(Solution().longestPalindromeSubseq("cac"))
+        return int(DP[0][-1])
+
+print(Solution().longestPalindromeSubseq("bbbab"))
