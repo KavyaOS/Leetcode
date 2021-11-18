0eb3e5f17bd6705230c539917ff6cc78585374bc
Kavya O S <kavya.os15@gmail.com>
2021-11-18 09:58:59 -0800

    solved as per topological sorted order

--- Algorithms/Graph_Algos/course_schedule2.py
+++ Algorithms/Graph_Algos/course_schedule2.py
@@ -1,33 +1,41 @@
 from collections import defaultdict
 class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        degree = [0] * numCourses
-        adj_list = [[] for x in range(numCourses)]
-        queue = []
-        
-        for course1, course2 in prerequisites:
-            degree[course1] = degree[course1] + 1
-            adj_list[course2].append(course1)
-            
-        for k in range(numCourses):
-            if degree[k] == 0:
-                queue.append(k)
-                
-        counter = 0
-        topological_sorted_order = []
-        
-        while queue:
-            key = queue.pop()
-            topological_sorted_order.append(key)
-            counter = counter + 1
-            for node in adj_list[key]:
-                degree[node] = degree[node] - 1
-                if degree[node] == 0:
-                    queue.append(node)
-                    
-        if counter!= numCourses:
-            return []
-        
-        return topological_sorted_order
+    pre = {}
+    post = {}
+    clock = 0
+    visited = {}
+    order = []
+    prerequisites = []
 
+    def has_cycle(self):
+        for u,v in self.prerequisites:
+            if self.post[u]<self.post[v]:
+                return True
+        return False
+
+    def neighbour(self, v):
+        return [u[1] for u in self.prerequisites if u[0] == v]
+
+    def explore(self, v):
+            self.visited[v] = True
+            self.pre[v] = self.clock
+            self.clock += 1
+            for u in self.neighbour(v):
+                if not self.visited[u]:
+                    self.explore(u)
+            self.post[v] = self.clock
+            self.order.append(v)
+            self.clock += 1
+
+    def findOrder(self, numCourses, prerequisites):
+        self.visited = {i:False for i in range(numCourses)}
+        self.prerequisites = prerequisites
+        self.order = []
+        for u in range(numCourses):
+            if not self.visited[u]:
+                self.explore(u)
+        if self.has_cycle():
+            return []
+        return self.order
+    
 print(Solution().findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
